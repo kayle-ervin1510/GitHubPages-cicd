ecea132d5b8bc4da93b6cff305fae5aec55cf9f3
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-07-23 13:15:40 -0700

    pushing it forward

--- pages/AboutPage.jsx
+++ pages/AboutPage.jsx
@@ -2,6 +2,11 @@ import { useState } from "react"
 
 export default function AboutPage() {
 
+    const [shouldCrash, setShouldCrash]=useState(false)
+    if (shoudCrash) {
+        throw new Error ("I told you not to press it!! This is an intentional crassh error!")
+    }
+
     return (
         <>
     
@@ -9,6 +14,12 @@ export default function AboutPage() {
         <p>
             <a href="https://www.imdb.com/title/tt2861424/" target="_blank" rel="nonreferrer" target="_blank" rel="nonreferrer"> Click To Read More!</a>
         </p>
+        <button 
+        className="rounded-md bg-red-500"
+        onClick={()=> setShouldCrash(true)}
+        > 
+        Don't Press It...
+        </button>
         </>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css"
 import { Outlet } from "react-router-dom"
 import { useState, useEffect } from "react"
-import HomePage from "../pages/HomePage.jsx"
+//import HomePage from "../pages/HomePage.jsx"
 import Heading from "./components/Heading/Heading.jsx"
 //import AboutPage from "../pages/AboutPage.jsx"
 import NavBar from "./components/NavBar/NavBar.jsx"
 
 
 function App() {
+
+    const[user, setUser] = useState(null)
+    const [count, setCount] = useState(0)
+    const [favs, setFavs] = useState([])
+
   
   useEffect(
     ()=>{
@@ -15,32 +20,27 @@ function App() {
       return ()=>{console.log("page unmounted")}
     }, []
   )
-
-  const[user, setUser] = useState(null)
-
-  const [favs, setFavs] = useState([])
-
-
-
-
+  
   useEffect(()=>{
     console.log(user)
-  }, [ user ]
-)
+  }, [user]
+  )
 
   return (
-    <>
-    <Heading/>
-    <HomePage/>
+    <>    
     <NavBar/>
-    <section>
+    <main className="min-h-screen bg-slate-100 p-8">
+      <div className="mx-auto max-w-5xl space-y-8">
+        <Heading/>
+        <Outlet context={ {favs, setFavs, count, setCount} }/>
+        <button onClick={()=>setUser("I am User!")}>Change User</button>
       <h3>
         <li>Watch the show here!</li>
         <a href="https://www.youtube.com/watch?v=BFTSrbB2wII" target="_blank" rel="nonreferrer"> Click Here!</a>
       </h3>
-      <button onClick={()=>setUser("I am User!")}>Change User</button>
       
-    </section>
+      </div>
+    </main>
     
     </>
   )
